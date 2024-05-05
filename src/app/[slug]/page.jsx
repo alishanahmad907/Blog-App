d10d3a828590575111200fde9af8fcbf21e7dadd
Ali Shan Ahmad <alishanahmad907@gmail.com>
2024-05-06 02:42:22 +0530

    frontend design complete kr diya

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./singlePage.module.css";
+import Menu from "@/components/Menu/Menu";
+import Image from "next/image";
+import Comments from "@/components/comments/Comments";
+const SinglePage = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.infoContainer}>
+				<div className={styles.textContainer}>
+					<h1 className={styles.title}>Lorem, ipsum dolor sit amet consectetur adipisicing.</h1>
+					<div className={styles.user}>
+						<div className={styles.userImageContainer}>
+							<Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
+						</div>
+						<div className={styles.userTextContainer}>
+							<span className={styles.username}>John Doe</span>
+							<span className={styles.date}> 01.01.2024</span>
+						</div>
+					</div>
+				</div>
+				<div className={styles.imageContainer}>
+					<Image src="/p1.jpeg" alt="" fill className={styles.image} />
+				</div>
+			</div>
+			<div className={styles.content}>
+				<div className={styles.post}>
+					<div className={styles.description}>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptatem asperiores, ipsum hic debitis veritatis? Alias at quia provident et aut deserunt dolores excepturi placeat nesciunt cum? Vitae, quos rerum.</p>
+						<h2>Headers </h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptatem asperiores, ipsum hic debitis veritatis? Alias at quia provident et aut deserunt dolores excepturi placeat nesciunt cum? Vitae, quos rerum.</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptatem asperiores, ipsum hic debitis veritatis? Alias at quia provident et aut deserunt dolores excepturi placeat nesciunt cum? Vitae, quos rerum.</p>
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta debitis laudantium quibusdam ad, voluptatem laborum rerum expedita qui fugiat.</p>
+					</div>
+                    <div className={styles.comment}>
+                        <Comments/>
+                    </div>
+				</div>
+				<Menu />
+			</div>
+		</div>
+	);
+};
+
+export default SinglePage;
